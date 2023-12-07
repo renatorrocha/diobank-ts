33731628fd522cf1431b97dbcae59f5a28e82072
renatorrodrigues <renatorrodrigues2002@gmail.com>
2023-12-07 09:46:28 -0300

    chore: implements all the class functions

--- app.ts
+++ app.ts
@@ -1,17 +1,28 @@
+// Feito por Renatorrocha
+
 import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
-import { SpecialAccount } from './class/SpecialAccount'
+import { VipAccount } from './class/VipAccount'
 
+console.log(
+  '******************************** PeopleAccount ********************************'
+)
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 1)
-peopleAccount.deposit(10)
 peopleAccount.deposit(20)
-peopleAccount.withdraw(40)
+peopleAccount.withdraw(10)
 
+console.log(
+  '******************************** CompanyAccount ********************************'
+)
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
 companyAccount.deposit(50)
+companyAccount.withdraw(10)
 companyAccount.getLoan(300)
-console.log(companyAccount)
 
-const specialAccount: SpecialAccount = new SpecialAccount('Renato', 2)
-specialAccount.SpecialDeposit(10)
-console.log(specialAccount)
+console.log(
+  '******************************** VipAccount ********************************'
+)
+const vipAccount: VipAccount = new VipAccount('Renato', 2)
+vipAccount.VipDeposit(10)
+vipAccount.deposit(20)
+vipAccount.withdraw(20)
