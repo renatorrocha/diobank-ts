423358f50dff05f376a6548ef75c9fb47d252070
renatorrodrigues <renatorrodrigues2002@gmail.com>
2023-12-07 08:52:22 -0300

    feat: deposit and withdraw

--- app.ts
+++ app.ts
@@ -1,9 +1,10 @@
 import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 1)
+peopleAccount.deposit(10)
+peopleAccount.deposit(20)
+peopleAccount.withdraw(40)
+// const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
+// companyAccount.deposit()
+// console.log(companyAccount)
--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,10 +1,10 @@
 export abstract class DioAccount {
   private name: string
   private readonly accountNumber: number
-  balance: number = 0
+  private balance: number = 0
   private status: boolean = true
 
-  constructor(name: string, accountNumber: number){
+  constructor(name: string, accountNumber: number) {
     this.name = name
     this.accountNumber = accountNumber
   }
@@ -18,14 +18,23 @@ export abstract class DioAccount {
     return this.name
   }
 
-  deposit = (): void => {
-    if(this.validateStatus()){
-      console.log('Voce depositou')
-    }
+  deposit = (value: number): void => {
+    this.balance = this.balance + value
+    console.log(
+      `Você depositou ${value}, seu saldo atual é de R$${this.balance}`
+    )
   }
 
-  withdraw = (): void => {
-    console.log('Voce sacou')
+  withdraw = (value: number): void => {
+    if (this.validateStatus() && this.balance > value) {
+      this.balance = this.balance - value
+    } else {
+      console.log(
+        `Saldo insuficiente para fazer o saque !\n Faltam ${
+          value - this.balance
+        } para fazer o saque desejado`
+      )
+    }
   }
 
   getBalance = (): void => {
